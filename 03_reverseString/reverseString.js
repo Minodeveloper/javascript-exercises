7ece891ff60b2018b9826db920966016a320571c
madhuresh <minoshideveloper8@gmail.com>
2022-05-17 18:00:14 +0530

    test 3 passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,18 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let returnedString = '';
+    let i =string.length-1;
+    while(i >=0)
+    {
+        
+        returnedString += string[i];
+        
+        
+        i--;
+    }
+    
+    return returnedString;
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
