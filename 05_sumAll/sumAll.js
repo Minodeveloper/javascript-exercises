5d1137a4de58a2808af8b7412d7ffcd64a095e03
madhuresh <minoshideveloper8@gmail.com>
2022-05-17 18:37:11 +0530

    passed test 6

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,35 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+    let array = [];
+    if(num1 < 0 || num2 <0 || typeof (num2+num1) != 'number' ) return 'ERROR';
+    if(num1 < num2)
+    {
+    for (let i = num1; i < (num2+1); i++)
+    {
+        array.push(i);
+    }
+    }else{
+        
+        
+        for (let i = num2; i < (num1+1); i++)
+        {
+            array.push(i);
+        }
+    }
+    let i = 0
+    let sum =0;
+    while(i < array.length)
+    {
+            sum +=array[i];
+            i++;
+    }
 
+return sum;
 };
 
 // Do not edit below this line
+
+// let arr = []
+// arr.push(6);
+// console.log(arr);
+// console.log(sumAll(1,4));
 module.exports = sumAll;
